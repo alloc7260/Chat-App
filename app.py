3f32570f598159046fcc0c4f6dddd02bdce80804
Pranav Hirani <pranav.hirani@ajmerainfotech.com>
2025-01-03 16:01:17 +0530

    all chat app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, make_response
 from flask_pymongo import PyMongo
 from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
@@ -12,6 +12,7 @@ app.config['SECRET_KEY'] = 'febghbn23u48934jfi3j4dj394u345r23'
 mongo = PyMongo(app)
 users_collection = mongo.db.users
 todos_collection = mongo.db.todos
+chats_collection = mongo.db.chats
 
 # check connection mongoDB
 @app.route('/check')
@@ -23,7 +24,7 @@ def check():
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        token = request.headers.get('x-access-tokens')
+        token = request.cookies.get('token')
         if not token:
             return jsonify({'message': 'Token is missing!'}), 403
         try:
@@ -49,8 +50,17 @@ def login():
     user = users_collection.find_one({'username': data['username']})
     if not user or not check_password_hash(user['password'], data['password']):
         return jsonify({'message': 'Login failed!'}), 401
-    token = jwt.encode({'username': user['username'], 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
-    return jsonify({'token': token})
+    token = jwt.encode({'username': user['username'], 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=10)}, app.config['SECRET_KEY'])
+    resp = make_response(jsonify({'message': 'Login successful!'}))
+    resp.set_cookie('token', token, httponly=True)
+    return resp
+
+@app.route('/logout', methods=['POST'])
+@token_required
+def logout(current_user):
+    resp = make_response(jsonify({'message': 'Logged out!'}))
+    resp.set_cookie('token', '', expires=0)
+    return resp
 
 @app.route('/todo', methods=['POST'])
 @token_required
@@ -65,6 +75,25 @@ def get_todos(current_user):
     todos = todos_collection.find({'user_id': current_user['_id']})
     output = [{'title': todo['title'], 'description': todo['description']} for todo in todos]
     return jsonify({'todos': output})
+
+@app.route('/chat', methods=['POST'])
+@token_required
+def create_chat(current_user):
+    data = request.get_json()
+    chats_collection.insert_one({'user_id': current_user['_id'], 'username': current_user['username'], 'message': data['message']})
+    return jsonify({'message': 'Message sent!'}), 201
+
+@app.route('/chat', methods=['GET'])
+@token_required
+def get_chats(current_user):
+    chats = chats_collection.find()
+    output = [{'username': chat['username'], 'message': chat['message']} for chat in chats]
+    return jsonify({'chats': output})
+
+@app.route('/dashboard')
+@token_required
+def dashboard(current_user):
+    return render_template('index.html')
 
 @app.route('/')
 def index():
