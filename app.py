a65b9ea6efd4c4bbc00b0ddfea17f434eb2a77d2
Pranav Hirani <pranav.hirani@ajmerainfotech.com>
2025-01-03 15:49:24 +0530

    initial working files

---
+++ app.py
@@ -0,0 +1,74 @@
+from flask import Flask, request, jsonify, render_template
+from flask_pymongo import PyMongo
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+import datetime
+from functools import wraps
+
+app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb://admin:example@localhost:27017/todoDB?authSource=admin'
+app.config['SECRET_KEY'] = 'febghbn23u48934jfi3j4dj394u345r23'
+
+mongo = PyMongo(app)
+users_collection = mongo.db.users
+todos_collection = mongo.db.todos
+
+# check connection mongoDB
+@app.route('/check')
+def check():
+    # list databases    
+    print(mongo.db.list_collection_names())
+    return jsonify({'message': 'Connected to MongoDB!'})
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.headers.get('x-access-tokens')
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 403
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+            current_user = users_collection.find_one({'username': data['username']})
+        except:
+            return jsonify({'message': 'Token is invalid!'}), 403
+        return f(current_user, *args, **kwargs)
+    return decorated
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    if not data or not data.get('username') or not data.get('password'):
+        return jsonify({'message': 'Invalid data!'}), 400
+    hashed_password = generate_password_hash(data['password'], method='sha256')
+    users_collection.insert_one({'username': data['username'], 'password': hashed_password})
+    return jsonify({'message': 'Registered successfully'}), 201
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = users_collection.find_one({'username': data['username']})
+    if not user or not check_password_hash(user['password'], data['password']):
+        return jsonify({'message': 'Login failed!'}), 401
+    token = jwt.encode({'username': user['username'], 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+    return jsonify({'token': token})
+
+@app.route('/todo', methods=['POST'])
+@token_required
+def create_todo(current_user):
+    data = request.get_json()
+    todos_collection.insert_one({'user_id': current_user['_id'], 'title': data['title'], 'description': data['description']})
+    return jsonify({'message': 'To-do created!'}), 201
+
+@app.route('/todo', methods=['GET'])
+@token_required
+def get_todos(current_user):
+    todos = todos_collection.find({'user_id': current_user['_id']})
+    output = [{'title': todo['title'], 'description': todo['description']} for todo in todos]
+    return jsonify({'todos': output})
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
